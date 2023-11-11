87d08e14a5aa6a5dce6bc52c4a1c1f38d166da3f
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2023-11-11 20:02:59 +0530

    update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ LOG = -1002122751557
 
 try:
   GROUPS =[]
-  for x in (os.environ.get('GROUPS', '-1002025716464').split())
+  for x in (os.environ.get('GROUPS', '-1002025716464').split()):
     GROUPS.append(int(x))
 except ValueError:
     raise Exception("Your AUTH GROUPS list does not contain valid integers.")    
