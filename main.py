54237d90388c54a4c90b4a63ca537c39c5c6a1c3
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2023-11-11 19:27:43 +0530

    update main.py

--- main.py
+++ main.py
@@ -144,6 +144,8 @@ async def account_login(bot: Client, m: Message):
             if "-n" in links[i]:
               url=v.split("-n")[0].strip()
               name = v.split("-n")[1].strip().replace("\t", "").replace(":", "").replace("/", "").replace("+", "")
+            else:
+              
             if "visionias" in url:
                 async with ClientSession() as session:
                     async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
