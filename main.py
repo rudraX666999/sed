6676c3da3708087c2c04cf7ef724c8a5c75b9da3
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2023-11-11 19:47:53 +0530

    update main.py

--- main.py
+++ main.py
@@ -192,8 +192,8 @@ async def account_login(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:                               
-                cc = f'** {str(count).zfill(3)}.** {name1} ({res}) .mkv\n**Batch Name :** {b_name}\n\n**Downloaded by : {CR}**'
-                cc1 = f'** {str(count).zfill(3)}.** {name1} .pdf \n**Batch Name :**{b_name}\n\n**Downloaded by : {CR}**'
+                cc = f'** {name_x}.** {name1} ({res}) .mkv\n\n**Downloaded by : {CR}**'
+                cc1 = f'** {name_x}.**pdf \n\n**Downloaded by : {CR}**'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
