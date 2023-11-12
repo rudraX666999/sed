35036103d58784873a7737e0bbfb5cf912885d63
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2023-11-12 08:12:59 +0530

    update helper.py

--- helper.py
+++ helper.py
@@ -113,7 +113,7 @@ async def send_vid(bot: Client, m: Message, cc, filename, thumb, name):
         f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"',
         shell=True)
    # await prog.delete(True)
-    rep = await m.reply_text(f"**♻️ Uploading ...** : `{filename}`")
+    rep = await m.reply_text(f"**♻️ Uploading ...**\n\n🥀 FileName »»  `{filename}`")
     try:
         if thumb == "no":
             thumbnail = f"{filename}.jpg"
