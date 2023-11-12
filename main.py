e0f988ee95c1ca2f058a7358b08e1f46de456679
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2023-11-12 08:06:30 +0530

    update main.py

--- main.py
+++ main.py
@@ -226,7 +226,7 @@ async def account_login(bot: Client, m: Message):
                         time.sleep(e.x)
                         continue
                 else:
-                    prog = await m.reply_text(f"**Downloading:-**\n\n** Video Name :-** `{name}\nQuality - {raw_text2}`\n**link:**`{url}`")
+                    prog = await m.reply_text(f"**Downloading »»  **\n\n** Video Name :-** `{name}\nQuality - {raw_text2}`\n**link:**`{url}`")
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
                     await prog.delete(True)
