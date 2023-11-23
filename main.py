6070f7c351238a567fa09acd822be0ed054bf8e9
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2023-11-23 17:26:43 +0530

    update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ async def account_login(bot: Client, m: Message):
             try:          
                 #cc = f'** {str(count).zfill(3)}.** {name1} ({res}) .mkv\n**Batch Name :** {b_name}\n\n**Downloaded by : {CR}**'
                 #cc1 = f'** {str(count).zfill(3)}.** {name1} .pdf \n**Batch Name :**{b_name}\n\n**Downloaded by : {CR}**'
-                cc = f'** {name_x.replace("_", " ")}**'
+                cc = f'** {name_x.replace("_", " ")}**\n'
                 if teacher:
                   cc+=f"\n👤 SIR »» **{teacher.strip()}**"
                 if date:
