dfbe7017d62cfa4b8f939d33a7c5cd0c2ba38cb3
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2024-01-25 13:22:34 +0530

    update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import os
 pat =  re.compile(r"(https?://+[\w\d:#@%/;$()~_?\+-=\\\.&]*)")
 
 bot = Client("bot",
-             bot_token= "6669926453:AAEZc_j2UETA96mOgXTCWCmoi4RMfcdoycg",
+             bot_token= "6945617327:AAG835T-JGGHUR0HCPs_pshSuU5trtdk6rM",
              api_id= 9741228,
              api_hash= "6f339e8fa827a5147fa2ff03898d610c")
 
