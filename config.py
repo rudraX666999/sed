973e03a431952b35c3d73ff9effd5a1bf77b6a2a
ArpitaX2 <148265807+ArpitaX2@users.noreply.github.com>
2024-02-05 09:14:11 +0530

    update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = API_ID = 9741228
 
 API_HASH = os.environ.get("API_HASH", "6f339e8fa827a5147fa2ff03898d610c")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6860427344:AAHvMP_VjrsCF4rbZzC4GtOw5aZ0jVddAHI")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6945617327:AAG835T-JGGHUR0HCPs_pshSuU5trtdk6rM")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
