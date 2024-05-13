8ee806d0960ff80b36b8ce81d9d0c3bc6a0a20cf
Molly Mitchell <mmitc103@asu.edu>
2024-05-13 09:36:38 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -66,7 +66,7 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
-# designed by 𝙄𝙉𝙑𝙄𝙓
+# designed by NOAH
 async def progress_bar(current, total, reply, start):
     if timer.can_send():
         now = time.time()
