39f1184ba52e906e7805bc4e501fd204a35bc30c
rudraX666999 <tiwariadhya5@gmail.com>
2024-06-27 00:11:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,27 @@
 import os
 
-API_ID = API_ID = 20959078
+API_ID = API_ID = 26484988
 
-API_HASH = os.environ.get("API_HASH", "b3dd1e7fa169aae46bb0d841519e1ab8")
+API_HASH = os.environ.get("API_HASH", "8d6e625fe3e296b4b6bd4817aca39ec5")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6915553639:AAEwtoaGyoZFvzNUGZG6U3JruFvdLli0t5U")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6409607893:AAEgEPIlrpGOfJIKsg9ZENOQwUBARKTDZPA")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6701527422))
+OWNER = int(os.environ.get("OWNER", 7473123628))
 
-LOG = -1002065884296
+LOG = -4275690616
 
 try:
   GROUPS =[]
-  for x in (os.environ.get('GROUPS', '-1002065884296 -1002014730016').split()):
+  for x in (os.environ.get('GROUPS', '-4275690616 -4278144230').split()):
     GROUPS.append(int(x))
 except ValueError:
     raise Exception("Your AUTH GROUPS list does not contain valid integers.")    
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6940485320 6224785606").split()):
+    for x in (os.environ.get("ADMINS", " 7473123628 6888054311 ").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
